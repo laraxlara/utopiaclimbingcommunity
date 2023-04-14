c8266ad9057d667d5c2f74a5d0ff6a1b326ffb83
unknown <laraxlara97@gmail.com>
2023-04-14 13:51:47 +0200

    typescript

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable  @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable  @typescript-eslint/no-unsafe-assignment @typescript-eslint/no-misused-promises */
 
 import React, { useState } from "react";
 import * as yup from "yup";
