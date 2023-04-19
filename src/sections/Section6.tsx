fcd42dd523062c069ab60923941cdd3012bcfd53
unknown <laraxlara97@gmail.com>
2023-04-19 16:37:51 +0200

    Overflow hidden

--- src/sections/Section6.tsx
+++ src/sections/Section6.tsx
@@ -7,9 +7,9 @@ const Section6 = () => {
   return (
     <section
       id="contact-section"
-      className="section w-full h-[100%] xl:h-[100vh] bg-black flex justify-center items-center"
+      className="section w-full h-[100%] xl:h-[100vh] bg-black flex justify-center items-center overflow-y-hidden"
     >
-      <div className="max-w-screen-xl my-0 mx-auto py-8 lg:py-16 px-4 max-w-screen-md flex flex-col xl:flex-row justify-between items-center gap-10">
+      <div className="max-w-screen-xl my-0 mx-auto py-8 lg:py-16 px-4 max-w-screen-md flex flex-col xl:flex-row justify-between items-center gap-10 overflow-y-hidden">
         <div className="max-w-4xl mb-10 xl:mb-0">
           <ContactForm />
         </div>
