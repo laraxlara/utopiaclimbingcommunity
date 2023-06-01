c66f7217e7b0f0a6c19a4b7374b5764aeda661d4
unknown <laraxlara97@gmail.com>
2023-06-01 11:02:14 +0200

    Email changed for nodemailer

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -36,8 +36,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<Response>) => {
     }
 
     await transporter.sendMail({
-      sender: `Utopia Website ${email}`,
-      to: "laraxlara97@gmail.com",
+      to: "utopia.climbingcommunity@gmail.com",
       from: `${email}`,
       replyTo: email,
       subject: `Utopia Climbing Community: ${subject}`,
